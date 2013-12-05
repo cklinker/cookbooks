b9769819a803992b3640a91df6f8cef3d7c6fef6
Craig Klinker <cklinker@rzware.com>
2013-12-04 23:04:16 -0700

    recipe changes

--- postfix/recipes/setup.rb
+++ postfix/recipes/setup.rb
@@ -17,3 +17,13 @@ template 'opendkim private key' do
   notifies :restart, resources(:service => 'opendkim')
 end
 
+template 'opendkim known hosts' do
+  path '/etc/opendkimhosts'
+  source 'opendkimhosts.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  backup false
+  notifies :restart, resources(:service => 'opendkim')
+end
+
