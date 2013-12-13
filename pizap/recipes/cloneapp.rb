594ce7695298974833f42dc037b6a4c60aff3d21
Craig Klinker <cklinker@rzware.com>
2013-12-12 23:53:46 -0700

    clone

---
+++ pizap/recipes/cloneapp.rb
@@ -0,0 +1,18 @@
+template 'id_rsa' do
+  path '/home/ubuntu/.ssh/id_rsa'
+  source 'id_rsa.erb'
+  owner 'ubuntu'
+  group 'ubuntu'
+  mode 0600
+  backup false
+end
+
+directory '/mnt/code' do
+  user "ubuntu"
+  group "www-data"
+end
+git "/mnt/code" do
+  repository "git@github.com:pizap/production-web.git"
+  user "ubuntu"
+  group "www-data"
+end
