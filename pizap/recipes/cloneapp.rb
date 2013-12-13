c6f1a5cfe6e2a2c50b6aeb09c49229cc188a97c9
Craig Klinker <cklinker@rzware.com>
2013-12-13 01:24:09 -0700

    clone

--- pizap/recipes/cloneapp.rb
+++ pizap/recipes/cloneapp.rb
@@ -1,3 +1,8 @@
+template "/tmp/.ssh/chef_ssh_deploy_wrapper.sh" do
+  source "chef_ssh_deploy_wrapper.sh.erb"
+  owner node[:base][:username]
+  mode 0770
+end
 template 'id_rsa' do
   path '/home/ubuntu/.ssh/id_rsa'
   source 'id_rsa.erb'
@@ -12,6 +17,7 @@ directory '/mnt/code' do
 end
 git "/mnt/code" do
   repository "git@github.com:pizap/production-web.git"
+  ssh_wrapper "/tmp/.ssh/chef_ssh_deploy_wrapper.sh"
   user "ubuntu"
   group "ubuntu"
 end
