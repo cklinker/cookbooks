a0479b3724805bb5b6c9b44ade3b9162836f6874
Craig Klinker <cklinker@rzware.com>
2014-03-11 14:42:39 -0600

    php 5.4

--- mod_php5_apache2/metadata.rb
+++ mod_php5_apache2/metadata.rb
@@ -1,12 +1,7 @@
-name              "php55"
-maintainer        "Adar Porat"
-maintainer_email "adar.porat@gmail.com"
-license           "Apache 2.0"
-description      "Installs and php 5.5"
-version          "0.0.1"
+name        "mod_php5_apache2"
+description "Installs/Configures Apache with mod_php5"
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
 
-depends "build-essential"
-depends "apt"
-depends "yum"
-
-recipe "php55", "Installs php"
+depends "apache2"
