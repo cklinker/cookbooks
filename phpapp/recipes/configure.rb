f3dc397fbdf5a9c9e0454901bb3a6e5efc67aaa6
Craig Klinker <cklinker@rzware.com>
2014-03-11 22:08:40 -0600

    update params

--- phpapp/recipes/configure.rb
+++ phpapp/recipes/configure.rb
@@ -1,4 +1,3 @@
-    include_recipe "apt"
 
     execute "python-software-properties" do
       command "apt-get install -y python-software-properties"
@@ -6,13 +5,11 @@
     end
 
     execute "install php55" do
-      #command "apt-get install -y php5"
       command "add-apt-repository ppa:ondrej/php5 && apt-get update -y && apt-get install -y php5 php5-curl"
       action :run
     end
 
     execute "restart apache2" do
-      #command "apt-get install -y php5"
       command "service apache2 restart"
       action :run
     end
