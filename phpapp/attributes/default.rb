9c82f99872ac817fc7d461c80b75197f177bb72a
Craig Klinker <cklinker@rzware.com>
2014-03-11 21:37:23 -0600

    update repo

--- phpapp/attributes/default.rb
+++ phpapp/attributes/default.rb
@@ -5,3 +5,9 @@ default[:phpapp][:timezone] = 'UTC'
 node.default['phpapp']['deploy']['user'] = "root"
 node.default['phpapp']['deploy']['group'] = "root"
 node.default['phpapp']['deploy']['home'] = "/root/"
+
+case node["platform_family"]
+  when "ubuntu"
+    node.default['php55']['dotdeb']['uri'] = "http://packages.dotdeb.org"
+    node.default['php55']['dotdeb']['distribution'] = "precise"
+end
