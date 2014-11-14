feb3772b8e81c383a32983efcd5ec8aaccb72a9e
Craig Klinker <cklinker@rzware.com>
2014-11-13 23:24:46 -0700

    webserver config

--- pizap/recipes/ephemeral.rb
+++ pizap/recipes/ephemeral.rb
@@ -12,3 +12,12 @@ template 'config_web' do
   mode 0644
   backup false
 end
+
+template 'ports_conf' do
+  path '/etc/apache2/ports.conf'
+  source 'ports.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  backup false
+end
