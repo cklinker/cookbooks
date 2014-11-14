f6753385077ac97cd0ea666fd46cdef3723adb4b
Craig Klinker <cklinker@rzware.com>
2014-11-13 23:07:16 -0700

    add website config

--- pizap/recipes/ephemeral.rb
+++ pizap/recipes/ephemeral.rb
@@ -3,3 +3,12 @@ directory '/mnt/ephemeral' do
   group "ubuntu"
   mode "0777"
 end
+
+template 'config_web' do
+  path '/etc/apache2/sites-available/deploy_to_web.conf'
+  source 'slim_conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  backup false
+end
